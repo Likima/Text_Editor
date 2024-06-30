e3e3e5496d95725c0b57b6867e57e86b7608a431
Brandon's Zenbook <tangbrandonk@gmail.com>
2024-06-30 00:26:16 -0700

    Updated Makefile to support SDL2

--- src/main.cpp
+++ src/main.cpp
@@ -53,10 +53,9 @@ int main() {
 
     GLuint shaderProgram = createShaderProgram(vertexSource, fragmentSource);
 
-    glUseProgram(shaderProgram);
-
     // Playing around with vertices
     unsigned int VAO;
+
     glGenVertexArrays(1, &VAO);
 
     glBindVertexArray(VAO);
@@ -64,6 +63,11 @@ int main() {
     glBindBuffer(GL_ARRAY_BUFFER, VAO);
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
+    glVertexAttribPointer(0,3,GL_FLOAT,GL_FALSE,3*sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
+    
+    glUseProgram(shaderProgram);
+    glDrawArrays(GL_TRIANGLES,0,3);
 
     // Main loop
     while (!glfwWindowShouldClose(window)) {
